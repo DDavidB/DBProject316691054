2ee7a13a5d5d971b35f83456b2bd4f24716b9882
DDavidB <102032525+DDavidB@users.noreply.github.com>
2026-05-12 15:46:39 +0300

    Add files via upload

---
+++ שלב א/dropTables.sql
@@ -0,0 +1,7 @@
+-- מחיקת טבלאות בסדר הפוך לתלות ביניהן
+DROP TABLE IF EXISTS Payments;
+DROP TABLE IF EXISTS Prescriptions;
+DROP TABLE IF EXISTS Visits_Records;
+DROP TABLE IF EXISTS Appointments;
+DROP TABLE IF EXISTS Doctors;
+DROP TABLE IF EXISTS Patients;
---
+++ שלב א/selectAll.sql.sql
@@ -0,0 +1,7 @@
+-- בדיקת נתונים בכל הטבלאות
+SELECT * FROM Patients;
+SELECT * FROM Doctors;
+SELECT * FROM Appointments;
+SELECT * FROM Visits_Records;
+SELECT * FROM Prescriptions;
+SELECT * FROM Payments;
